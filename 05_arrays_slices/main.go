286b4a44fac3d276a0a11c98a452c3fa0b4afd94
Charlie <charles.mulic@gmail.com>
2019-01-08 10:27:34 -0600

    adding arrays

---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// arrays have to be a fixed length and known types
+	// slices are basically arrays without fixed length
+
+	// var fruitArr [2]string
+
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Orange"
+
+	// fruitArr := [2]string{"Apple", "Orange"} // declare and init shorthand
+	fruitArr := []string{"Apple", "Orange", "Grape", "Cherry"} // declare and init shorthand
+
+	fmt.Println(fruitArr)
+	fmt.Println(len(fruitArr)) // get length
+	fmt.Println(fruitArr[1:3]) // get a range
+	fmt.Println(fruitArr[1])
+}
